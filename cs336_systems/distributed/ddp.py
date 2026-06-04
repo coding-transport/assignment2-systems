eda48d7f43480b8ffdbd481946e558db447f1b9f
Yif Wang Linux <2247138160@qq.com>
2026-06-04 18:04:45 +0800

    start

--- cs336_systems/distributed/ddp.py
+++ cs336_systems/distributed/ddp.py
@@ -2,3 +2,9 @@
 # @Time : 2026/5/12 17:05
 # @Author : Yif Wang
 # @file : ddp
+import torch
+import torch.distributed as dist
+
+class ddp(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
